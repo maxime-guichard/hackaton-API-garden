c3efec9871bb1230e5be15ad3da3fbd8900dd909
endricklosange <endricklosange@gmail.com>
2021-05-11 23:11:04 +0200

    1.0

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,18 +23,44 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        /*        $client = HttpClient::create();
-        $response = $client->request('GET', 'http://www.themealdb.com/api/json/v1/1/random.php');
+        $client = HttpClient::create();
+        $responseMonday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52781');
+        $responseTuesday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52782');
+        $responseWednesday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52783');
+        $responseThursday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52780');
+        $responseFriday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52784');
+        $responseSaturday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52785');
+        $responseSunday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52786');
 
-        $statusCode = $response->getStatusCode(); // get Response status code 200
+        $statusCode = $responseMonday->getStatusCode(); // get Response status code 200
+        $statusCode = $responseTuesday->getStatusCode();
+        $statusCode = $responseWednesday->getStatusCode();
+        $statusCode = $responseThursday->getStatusCode();
+        $statusCode = $responseFriday->getStatusCode();
+        $statusCode = $responseSaturday->getStatusCode();
+        $statusCode = $responseSunday->getStatusCode();
 
         if ($statusCode === 200) {
-            $tests = $response->toArray();
+            $tests = $responseMonday->toArray();
+            $tests1 = $responseTuesday->toArray();
+            $tests2 = $responseWednesday->toArray();
+            $tests3 = $responseThursday->toArray();
+            $tests4 = $responseFriday->toArray();
+            $tests5 = $responseSaturday->toArray();
+            $tests6 = $responseSunday->toArray();
+        
             // convert the response (here in JSON) to an PHP array
         }
-        var_dump($tests);
-*/
-        return $this->twig->render('Home/index.html.twig');
+
+        return $this->twig->render('Home/index.html.twig',[
+            'tests'  =>  $tests,
+            'tests1' => $tests1,
+            'tests2' => $tests2,
+            'tests3' => $tests3,
+            'tests4' => $tests4,
+            'tests5' => $tests5,
+            'tests6' => $tests6
+        ]);
     }
     public function recipe()
     {
