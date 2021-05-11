fe87c7c11db362682be42f7b63149c91cb0a7e66
Maxime Guichard <maxime.guichard45@gmail.com>
2021-05-11 15:14:51 +0200

    ajout card pour recette

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpClient\HttpClient;
+//use Symfony\Component\HttpClient\HttpClient;
 
 class HomeController extends AbstractController
 {
@@ -23,7 +23,7 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        $client = HttpClient::create();
+        /*        $client = HttpClient::create();
         $response = $client->request('GET', 'http://www.themealdb.com/api/json/v1/1/random.php');
 
         $statusCode = $response->getStatusCode(); // get Response status code 200
@@ -33,7 +33,7 @@ class HomeController extends AbstractController
             // convert the response (here in JSON) to an PHP array
         }
         var_dump($tests);
-
-                return $this->twig->render('Home/index.html.twig',['tests' => $tests]);
-            }
+*/
+        return $this->twig->render('Home/index.html.twig');
     }
+}
