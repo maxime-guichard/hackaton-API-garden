9f19712baec89a20c5d9b10eac68f8a86626db7b
Serdar45000 <serdar45000@gmail.com>
2021-05-12 09:10:40 +0200

    FIX test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,8 +21,49 @@ class HomeController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function index(): string
+
+    public function index()
     {
-        return $this->twig->render('Home/index.html.twig');
+        $monday = $tuesday = $wednesday = '';
+        $client = HttpClient::create();
+        $responseMonday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771');
+        $responseTuesday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52770');
+        $responseWednesday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52776');
+
+        $statusCode = $responseMonday->getStatusCode();
+
+        if ($statusCode === 200) {
+            $monday = $responseMonday->toArray();
+            $tuesday = $responseTuesday->toArray();
+            $wednesday = $responseWednesday->toArray();
+        }
+
+        return $this->twig->render('Home/index.html.twig', [
+            'monday' => $monday, ['monday'],
+            'tuesday' => $tuesday, ['tuesday'],
+            'wednesday' => $wednesday, ['wednesday'],
+        ]);
+    }
+    public function recipe()
+    {
+        $monday = $tuesday = $wednesday = '';
+        $client = HttpClient::create();
+        $responseMonday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771');
+        $responseTuesday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52770');
+        $responseWednesday = $client->request('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52776');
+
+        $statusCode = $responseMonday->getStatusCode();
+
+        if ($statusCode === 200) {
+            $monday = $responseMonday->toArray();
+            $tuesday = $responseTuesday->toArray();
+            $wednesday = $responseWednesday->toArray();
+        }
+
+        return $this->twig->render('Home/recipes.html.twig', [
+            'monday' => $monday, ['monday'],
+            'tuesday' => $tuesday, ['tuesday'],
+            'wednesday' => $wednesday, ['wednesday'],
+        ]);
     }
 }
